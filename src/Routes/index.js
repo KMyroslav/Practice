5d1d714ea7d48b97972210340f5f94738cdbc7db
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-11-05 20:44:55 +0200

    init

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,15 @@
+import MoveElements from "./MoveElements/MoveElements";
+import InputToAsterisk from "./InputToAsterisk/InputToAsterisk";
+import PsyTest from "./PsyTest/PsyTest";
 import PetrolCalculator from "./PetrolCalc/PetrolCalc";
+import MarqueeLine from "./MarqueeLine/MarqueeLine";
+
 const routes = [
+  { MoveElements },
+  { InputToAsterisk },
+  { PsyTest },
   { PetrolCalculator },
+  { MarqueeLine },
 ];
+
 export default routes;
