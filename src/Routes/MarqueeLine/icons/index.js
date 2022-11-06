ba81183c88656813ccfb44c52926c264f739adf3
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-11-06 21:52:08 +0200

    Add icons

---
+++ src/Routes/MarqueeLine/icons/index.js
@@ -0,0 +1,71 @@
+import { ReactComponent as Axios } from "./axios.svg";
+import { ReactComponent as Bem } from "./bem.svg";
+import { ReactComponent as Css } from "./css-3.svg";
+import { ReactComponent as Express } from "./express.svg";
+import { ReactComponent as Handlebars } from "./handlebars.svg";
+import { ReactComponent as Html } from "./html-5.svg";
+import { ReactComponent as Javascript } from "./javascript.svg";
+import { ReactComponent as Mongodb } from "./mongodb-icon.svg";
+import { ReactComponent as Nodejs } from "./nodejs-icon.svg";
+import { ReactComponent as Parcel } from "./parcel-icon.svg";
+import { ReactComponent as ReactRouter } from "./react-router.svg";
+import { ReactComponent as React } from "./react.svg";
+import { ReactComponent as Redux } from "./redux.svg";
+import { ReactComponent as Sass } from "./sass.svg";
+import { ReactComponent as Socket } from "./socket.io.svg";
+import { ReactComponent as Webpack } from "./webpack.svg";
+
+const icons = [
+  Javascript,
+  Css,
+  Html,
+  Bem,
+  Express,
+  Mongodb,
+  Axios,
+  Nodejs,
+  Handlebars,
+  Parcel,
+  Webpack,
+  ReactRouter,
+  React,
+  Redux,
+  Sass,
+  Socket,
+];
+
+export default icons;
+
+// function Li0() {
+//   return <li>0</li>;
+// }
+// function Li1() {
+//   return <li>1</li>;
+// }
+// function Li2() {
+//   return <li>2</li>;
+// }
+// function Li3() {
+//   return <li>3</li>;
+// }
+// function Li4() {
+//   return <li>4</li>;
+// }
+// function Li5() {
+//   return <li>5</li>;
+// }
+// function Li6() {
+//   return <li>6</li>;
+// }
+// function Li7() {
+//   return <li>7</li>;
+// }
+// function Li8() {
+//   return <li>8</li>;
+// }
+// function Li9() {
+//   return <li>9</li>;
+// }
+// const icons = [Li0, Li1, Li2, Li3, Li4, Li5, Li6, Li7, Li8, Li9];
+
+// export default icons;
